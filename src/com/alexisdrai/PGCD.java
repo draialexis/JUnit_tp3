228ad1c94714874f1675afecb5f0bc51a9dcc0fd
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-01-27 15:29:51 +0100

    :recycle: Ex2_optimize

--- src/com/alexisdrai/PGCD.java
+++ src/com/alexisdrai/PGCD.java
@@ -2,18 +2,7 @@ package com.alexisdrai;
 
 public class PGCD {
     public int pgcd(int a, int b) {
-        if (a == 9 && b == 6) {
-            return 3;
-        }
-        if (a == -9 && b == 3) {
-            return 3;
-        }
-        if (a == 1 && b == 23) {
-            return 1;
-        }
-        if (a == -42 && b == 1) {
-            return 1;
-        }
+
         if (a == 0 && b == -7) {
             return 7;
         }
