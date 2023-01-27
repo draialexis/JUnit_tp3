6ab4c9f6d85fb24b0dd8c10c9b04b4c3ffc7abff
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-01-27 14:41:07 +0100

    :construction: 1_6 & 1_7 & 1_8

--- tests/com/alexisdrai/FibonacciTests.java
+++ tests/com/alexisdrai/FibonacciTests.java
@@ -1,7 +1,10 @@
 package com.alexisdrai;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +21,13 @@ public class FibonacciTests {
     @BeforeEach
     void initEach() {
         fibonacci = new Fibonacci();
+    }
+
+    private static Random rdm;
+
+    @BeforeAll
+    static void initAll() {
+        rdm = new Random();
     }
 
     @Test
@@ -49,4 +59,78 @@ public class FibonacciTests {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void fibMinusOneThrows() {
+        // Arrange
+
+        // Act
+        int n = -1;
+
+        // Assert
+        Exception exc = assertThrows(ArithmeticException.class, () -> fibonacci.fib(n));
+        assertEquals(exc.getMessage(), "Fibonacci's sequence begins at n_0");
+    }
+
+    @Test
+    void fibTwoReturnsOne() {
+        // Arrange
+        int expected = 1;
+
+        // Act
+        int actual = fibonacci.fib(2);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibEightReturnsTwentyOne() {
+        // Arrange
+        int expected = 21;
+
+        // Act
+        int actual = fibonacci.fib(8);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibFortyOneReturnsClosestToMaxInt() {
+        // Arrange
+        int expected = 165_580_141;
+
+        // Act
+        int actual = fibonacci.fib(41);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void fibFortyTwoThrows() {
+        // Arrange
+
+        // Act
+        int n = 42;
+
+        // Assert
+        Exception exc = assertThrows(IllegalArgumentException.class, () -> fibonacci.fib(n));
+        assertEquals(exc.getMessage(), "fib(42) would exceed Integer.MAX_VALUE");
+    }
+
+    @Test
+    void fibNReturnsNMinOnePlusNMinTwo() {
+        for (int i = 0; i < 100; i++) {
+            // Arrange
+            int n = rdm.nextInt(42); // after fib(41), we're out of INT territory
+            int expected = fibonacci.fib(n - 1) + fibonacci.fib(n - 2);
+
+            // Act
+            int actual = fibonacci.fib(n);
+
+            // Assert
+            assertEquals(expected, actual);
+        }
+    }
 }
