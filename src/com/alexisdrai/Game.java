e62a7189e81c8fa71063e0a0e7350eca7eeffdd0
Alexis DRAI <alexis.drai@etu.uca.fr>
2023-02-07 16:32:54 +0100

    Add seeded UTs, make them compilable

--- src/com/alexisdrai/Game.java
+++ src/com/alexisdrai/Game.java
@@ -1,14 +1,16 @@
 package com.alexisdrai;
 
 public class Game {
-    private char[][] board;
+    private final char[][] board;
+    public Game() {
+        board = new char[][]{{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+    }
 
     public char[][] getBoard() {
-        return new char[0][];
+        return board;
     }
 
-    public void setBoard(char[][] inBoard) {
-    }
+    public void setBoard(char[][] inBoard) {}
 
     public boolean play(int x, int y) {
         return false;
@@ -25,4 +27,7 @@ public class Game {
     public char nextPlayer() {
         return 0;
     }
+
+    public int[] input() {return new int[]{0, 0};}
+    // will not be tested, uses Scanner and manual input, will be very simple
 }
--- tests/com/alexisdrai/GameTests.java
+++ tests/com/alexisdrai/GameTests.java
@@ -30,6 +30,11 @@ public class GameTests {
         assertInstanceOf(Game.class, game);
         assertEquals(3, game.getBoard().length); // rows
         assertEquals(3, game.getBoard()[0].length); // cols
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertEquals('_', game.getBoard()[i][j]); // should be initialized empty
+            }
+        }
     }
 
     @ParameterizedTest
@@ -194,7 +199,101 @@ public class GameTests {
         assertEquals(expected, actual);
     }
 
-    // TODO seed nextplayer
-    // TODO seed isover
-    // TODO seed fillboard
+    @Test
+    void gameNextPlayerRdm() {
+
+        for (int n = 0; n < 100; ++n) {
+            // Arrange
+            int nbX = rdm.nextInt(0, 6); // Max number of 'x' on grid, +1
+            int nbO = nbX - rdm.nextInt(0, 2); // number of 'x' - 0 or - 1
+
+            char expected;
+            if (nbX == nbO) {
+                expected = 'x';
+            }
+            else {
+                if (nbX == nbO + 1) { // if 'x' has played 1 more than 'o', then it's 'o' 's turn
+                    expected = 'o';
+                }
+                else {
+                    throw new RuntimeException("this should never happen");
+                }
+            }
+
+            int x, y, turn = 1;
+            while (nbX > 0 && nbO > 0) { // randomly fill the grid
+
+                x = rdm.nextInt(0, 3); // in grid ([0 ; 2])
+                y = rdm.nextInt(0, 3);
+                if (game.getBoard()[x][y] == '_') {
+                    game.play(x, y);
+                    if (turn % 2 == 1) { // 'x' 's turn
+                        --nbX;
+                    }
+                    else { // 'o' 's turn'
+                        --nbO;
+                    }
+                    turn++;
+                }
+            }
+
+            // Act
+            char actual = game.nextPlayer();
+
+            // Assert
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void gameIsOverRdm() {
+
+        for (int n = 0; n < 100; ++n) {
+            // Arrange
+            int nbX = rdm.nextInt(4, 6); // Max number of 'x' on grid, + 1
+            int nbO = nbX - rdm.nextInt(0, 2); // number of 'x' - 0 or - 1
+
+            boolean expected = nbX == 5 && nbO == 4;
+
+            int x, y, turn = 1;
+            while (nbX > 0 && nbO > 0) { // randomly fill the grid
+
+                x = rdm.nextInt(0, 3); // in grid ([0 ; 2])
+                y = rdm.nextInt(0, 3);
+                if (game.getBoard()[x][y] == '_') {
+                    game.play(x, y);
+                    if (turn % 2 == 1) { // 'x' 's turn
+                        --nbX;
+                    }
+                    else { // 'o' 's turn'
+                        --nbO;
+                    }
+                    turn++;
+                }
+            }
+
+            char[][] board = game.getBoard();
+            if (!expected) { // if not full, check for winner instead
+                // only 8 ways to win this
+                expected = (
+                        (board[0][0] == board[0][1] &&  board[0][1] == board [0][2])
+                        || (board[1][0] == board[1][1] &&  board[1][1] == board [1][2])
+                        || (board[2][0] == board[2][1] &&  board[2][1] == board [2][2])
+
+                        || (board[0][0] == board[1][0] &&  board[1][0] == board [2][0])
+                        || (board[0][1] == board[1][1] &&  board[1][1] == board [2][1])
+                        || (board[0][2] == board[1][2] &&  board[1][2] == board [2][2])
+
+                        || (board[0][0] == board[1][1] &&  board[1][1] == board [2][2])
+                        || (board[0][2] == board[1][1] &&  board[1][1] == board [2][0])
+                );
+            }
+
+            // Act
+            boolean actual = game.isOver();
+
+            // Assert
+            assertEquals(expected, actual);
+        }
+    }
 }
